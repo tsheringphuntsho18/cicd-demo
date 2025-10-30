d32373308fff0bd3f4caff0926453663b9ee92e6
Tshering Phuntsho <0223031.cst@rub.edu.bt>
2025-10-31 00:09:38 +0600

    fixed main

--- src/main/java/sg/edu/nus/iss/cicddemo/Controller/DataController.java
+++ src/main/java/sg/edu/nus/iss/cicddemo/Controller/DataController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.javafaker.Faker;
+import net.datafaker.Faker;
 
 @RestController
 public class DataController {
@@ -21,32 +21,30 @@ public class DataController {
 
     @GetMapping("/nations")
     public JsonNode getRandomNations() {
-        var objectMapper = new ObjectMapper();
-        var faker = new Faker();
-        var nations = objectMapper.createArrayNode();
-        for (var i = 0; i < 10; i++) {
-            var nation = faker.nation();
-            nations.add(objectMapper.createObjectNode()
-                    .put("nationality", nation.nationality())
-                    .put("capitalCity", nation.capitalCity())
-                    .put("flag", nation.flag())
-                    .put("language", nation.language()));
-        }
-        return nations;
+    var objectMapper = new ObjectMapper();
+    var faker = new Faker();
+    var nations = objectMapper.createArrayNode();
+    for (var i = 0; i < 10; i++) {
+        nations.add(objectMapper.createObjectNode()
+            .put("nationality", faker.nation().nationality())
+            .put("capitalCity", faker.nation().capitalCity())
+            .put("flag", faker.nation().flag())
+            .put("language", faker.nation().language()));
+    }
+    return nations;
     }
 
     @GetMapping("/currencies")
     public JsonNode getRandomCurrencies() {
-        var objectMapper = new ObjectMapper();
-        var faker = new Faker();
-        var currencies = objectMapper.createArrayNode();
-        for (var i = 0; i < 20; i++) {
-            var currency = faker.currency();
-            currencies.add(objectMapper.createObjectNode()
-                    .put("name", currency.name())
-                    .put("code", currency.code()));
-        }
-        return currencies;
+    var objectMapper = new ObjectMapper();
+    var faker = new Faker();
+    var currencies = objectMapper.createArrayNode();
+    for (var i = 0; i < 20; i++) {
+        currencies.add(objectMapper.createObjectNode()
+            .put("name", faker.currency().name())
+            .put("code", faker.currency().code()));
+    }
+    return currencies;
 
     }
 
